6fcd0fc0eb314d81a818bcd10e6f2c77ae4422ce
SevenSecrets <freddieshughes@googlemail.com>
2020-08-11 14:56:27 +0100

    initial commit (pretty much everything was premade)

---
+++ src/components/Project.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: props.name, description: props.description, siteLink: props.siteLink, gitHubLink: props.gitHubLink }
+    }
+
+    render() {
+        return(
+            <div className="Project">
+                <h4>{this.state.name}</h4>
+                <p>{this.state.description}</p>
+                {this.state.siteLink && <a className="project-link" href={this.state.siteLink} target="_blank" rel="noopener noreferrer">Website</a>}
+                <a className="project-link" href={this.state.gitHubLink} target="_blank"  rel="noopener noreferrer">GitHub</a>
+            </div>
+        );
+    }
+}
+
+export default Projects;

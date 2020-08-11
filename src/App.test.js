e43ac64e18136628d5e297df0868faa84ecc7a01
SevenSecrets <freddieshughes@googlemail.com>
2020-08-11 15:02:09 +0100

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Project Greenprint', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Project Greenprint/i);
+  expect(titleElement).toBeInTheDocument();
 });
